a621f6407923c9ff8fbecd6c388592634ae146de
alejandro <alegue1985@gmail.com>
2025-03-04 15:17:38 -0600

    Initial Commit

---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alex Guerra on 2/28/25.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
