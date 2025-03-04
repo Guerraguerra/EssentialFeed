9e56256f3cd4ddea4436c79bad4e309303afb110
alejandro <alegue1985@gmail.com>
2025-03-04 15:17:38 -0600

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alex Guerra on 3/3/25.
+//
+
+struct FeedItem{
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
