7ba686d51272b331037316e1afbdd69314e7ceb5
alejandro <alegue1985@gmail.com>
2025-03-06 21:51:19 -0600

    Request data from url on load

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -7,7 +7,35 @@
 
 import XCTest
 
+class RemoteFeedLoader{
+    func load(){
+        HTTPClient.shared.requestedURL = URL(string: "https://example.com/feed")!
+    }
+}
+
+class HTTPClient {
+    
+    static let shared = HTTPClient()
+    private init() {}
+    var requestedURL: URL?
+    
+}
+
 final class RemoteFeedLoaderTest: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -15,21 +43,6 @@ final class RemoteFeedLoaderTest: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
