3f1d86719fbd65258b3486b4bbc708796e47bbef
alejandro <alegue1985@gmail.com>
2025-06-08 16:19:16 -0500

    Move start/stop intercepting request to setUp/TearDown method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -27,22 +27,16 @@ class URLSessionHTTPClient{
 
 class URLSessionHTTPClientTests: XCTestCase {
 
-    
-    /*func test_getFromUrl_resumeDataTaskWithURL(){
-        let url = URL(string: "http://any-url.com")!
-        let session = HTTPSessionSpy()
-        let task = URLSessionDataTaskSpy()
-        session.stub(url: url, task: task)
-        let sut = URLSessionHTTPClient(session: session)
-        
-        sut.get(from: url){ _ in
-            
-        }
-        
-        XCTAssertEqual(task.resumeCallCount, 1)
-    }*/
-    func test_getFromURL_performGETRequestWithURL(){
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performGETRequestWithURL(){
         
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
@@ -55,11 +49,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLSessionHTTPClient().get(from: url) { _ in }
         wait(for: [exp], timeout: 1)
         
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromUrl_failsOnRequestError(){
-        URLProtocolStub.startInterceptingRequests()
+        
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "Any error", code: 1)
         URLProtocolStub.stub( data: nil, response: nil, error: error)
@@ -78,7 +71,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1)
-        URLProtocolStub.stopInterceptingRequests()
+        
     }
 
     // MARK: - Helpers
