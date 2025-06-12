d7deb397e52a8ecf5542bea898c86fda20652d3a
alejandro <alegue1985@gmail.com>
2025-06-12 15:56:11 -0500

    Extract test set-up to a helper method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,28 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPClient : HTTPClient{
-    private let session: URLSession
-    
-    struct UnexpectedValuesRepresentation: Error{}
-    
-    init(session: URLSession = .shared){
-        self.session = session
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        //let url = URL(string: "http://wrong-url.com")!  This line is to make the test test_getFromURL_performGETRequestWithURL fail
-        session.dataTask(with: url) { data, response, error in
-            if let error = error{
-                completion(.failure(error))
-            } else if let receivedData = data, let receivedresponse = response as? HTTPURLResponse{
-                completion(.success(receivedData, receivedresponse))
-            }else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
+
 
 class URLSessionHTTPClientTests: XCTestCase {
 
