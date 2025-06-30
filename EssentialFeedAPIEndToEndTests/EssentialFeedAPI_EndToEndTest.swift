3ef17e2f2d35f6092916ed58fe68a986ede9cfd7
alejandro <alegue1985@gmail.com>
2025-06-30 15:00:32 -0500

    update til test save request to cache deletion

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPI_EndToEndTest.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPI_EndToEndTest.swift
@@ -44,6 +44,7 @@ class EssentialFeedAPI_EndToEndTest: XCTestCase {
     }
     
     //MARK: - Helpers
+    
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
--- Feed Cashe/CacheFeedUseCaseTests.swift
+++ Feed Cashe/CacheFeedUseCaseTests.swift
@@ -6,30 +6,78 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class CacheFeedUseCaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    class LocalFeedLoader{
+        var store: FeedStore
+        init(store: FeedStore){
+            self.store = store
+        }
+        
+        func save(_ items: [FeedItem]){
+            store.deleteCachedFeed()
         }
     }
+    
+    class FeedStore{
+        var deleteCacheFeedCallCount = 0
+        
+        func deleteCachedFeed(){
+            deleteCacheFeedCallCount = 1
+        }
+    }
+    
+    func test_init_DoesNotDeleteCacheUponCreation(){
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCasheDeletion(){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueFeedItem(), uniqueFeedItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    private func uniqueFeedItem() -> FeedItem {
+        return FeedItem(
+            id: UUID(),
+            description: "Any description",
+            location: "Any location",
+            imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
+
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
