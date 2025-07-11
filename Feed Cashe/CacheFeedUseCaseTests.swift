adee2cd4557df06365c91d250fb4a81c20ad33bf
alejandro <alegue1985@gmail.com>
2025-07-10 22:51:24 -0500

    Save command succeeds on successful chache insertion

--- Feed Cashe/CacheFeedUseCaseTests.swift
+++ Feed Cashe/CacheFeedUseCaseTests.swift
@@ -66,6 +66,10 @@ final class CacheFeedUseCaseTests: XCTestCase {
         func completeInsertion(with error: Error, at index: Int = 0){
             insertionCompletions[index](error)
         }
+        
+        func completeInsertionSuccessfully(at index: Int = 0){
+            insertionCompletions[index](nil)
+        }
     }
     
     func test_init_DoesNotMessageStoreUponCreation(){
@@ -139,6 +143,21 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, insertionError)
     }
     
+    func test_save_succedsOnSuccessfulCacheInsertion(){
+        let (sut, store) = makeSUT()
+        let items = [uniqueFeedItem(), uniqueFeedItem()]
+        var receivedError: Error?
+        let exp = expectation(description: "Wait for save completion")
+        sut.save(items) {error in
+            receivedError = error
+            exp.fulfill()
+        }
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
+    }
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
