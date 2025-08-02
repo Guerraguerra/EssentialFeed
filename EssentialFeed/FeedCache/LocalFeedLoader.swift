daa55d7cdc4666e06a811b838d211ab394b9c561
alejandro <alegue1985@gmail.com>
2025-08-01 22:28:49 -0500

    Adding last modification that crash in the previous project

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+public class LocalFeedLoader{
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    public init(store: FeedStore, timestamp: @escaping () -> Date){
+        self.store = store
+        self.currentDate = timestamp
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void){
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            }
+            else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void){
+        store.insert(items, timestamp: currentDate()){ [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
